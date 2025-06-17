3dfe2b6c16d70d82cd5928c8b5e6f37b9acc779b
gaetano43 <gaetanocarpentiere71@gmail.com>
2025-06-17 23:19:05 +0200

    Delete src/main/java/com/example/Test/repository/AutoreRepository.java

--- src/main/java/com/example/Test/repository/AutoreRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.Test.repository;
-
-import com.example.Test.model.Autore;
-import org.springframework.data.repository.CrudRepository;
-
-public interface AutoreRepository extends CrudRepository <Autore,Integer> {
-}
